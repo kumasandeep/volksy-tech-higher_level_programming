cbb50e8f47b54d14b1a2cd46e098e95cd0a6a4d3
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-02 01:32:58 -0800

    l

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3   
-for i in range(ord('a'), ord('z') + 1):                                       
-    if i == ord('q') or i == ord('e'):
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print("{:c}".format(i), end="")
+    else:
+        print("{:s}".format(chr(i)), end="")
