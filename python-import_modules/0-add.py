3de28293b78cc89a173a297b0b1aa4bee988e67a
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-04 01:44:34 -0800

    j

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    from add_0 import add 
-    a = 1
-    b = 2
+from add_0 import add
+if _name_ == "_main_": 
+    a = 10
+    b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
