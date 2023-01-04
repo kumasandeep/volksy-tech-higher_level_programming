2f4dec5bcc74f4b06410df8fc140bb36ce14c51f
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-04 05:44:49 -0800

    l

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -21,4 +21,5 @@ else:
     exit(1)
 
 result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2], int(argv[3]), result))
+print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+     argv[2], int(argv[3]), result))
