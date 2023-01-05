80306fef44b1016368373aef24469d7bf4975bf7
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-05 03:36:04 -0800

    l

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx >= len(my_list) or idx < 0:
+        return replace_in_list
+    return replace_in_list
+    print(new_list)
+    print(my_list)
