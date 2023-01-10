f3146ccd37901d90e2801841c0b472980058f169
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-09 22:47:05 -0800

    l

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i], end='')
             integers += 1
-        except(TypeError, ValueError):
+        except (TypeError, ValueError):
             continue
-        except(IndexError):
+        except (IndexError):
             raise
     print()
     return integers
