73944d8b5f3e52891a2c6319e2e79e8b240cd7b8
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-11 05:02:47 -0800

    l

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+    """Size class of a square"""
+
+
+    class Square:
+        """Square class"""
+        def __init__(self, size):
+            """Size of square"""
+            self.__size = size
