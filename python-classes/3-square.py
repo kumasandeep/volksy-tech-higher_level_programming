576badf64b11bb0ac0ffc46b5abc745d0d0c44d2
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-11 05:23:39 -0800

    l

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Area of Square"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """Constructor"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+
+    def area(self):
+        """Area of Square"""
+        return(self.__size * self.__size)
