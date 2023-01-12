d5eb2190644504653d020f05de34a7d41c966bd3
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-11 20:55:29 -0800

    l

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""creating class """
+"""creates class Square with
+private instance attribute size and public instance method"""
 
 
 class Square:
-    """class for square"""
+    """defines class with instantiated and validated private instance attribute
+and public instance method."""
 
     def __init__(self, size=0):
-        """constructor"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
 
     def area(self):
-        """area for square"""
-        return  (self.__size * self.__size)
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
