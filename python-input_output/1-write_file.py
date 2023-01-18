999d771795ec28c4ab64dea2f9db2ce5d3de420c
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-18 02:46:49 -0800

    l

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,11 +6,12 @@
 
 def number_of_lines(filename=""):
     """Returns the number of lines of a text file.
+
     Keyword Arguments:
         filename {str} -- file name (default: {""})
+
     Returns:
         Int -- number of lines of a text file.
     """
-    
     with open(filename, mode="r", encoding="utf-8") as file:
         return len(file.readlines())
