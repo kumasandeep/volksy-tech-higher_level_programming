96c409638e0c8fe9bbe76e8fdd103c04bcd4e132
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-18 03:38:43 -0800

    k

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""This module contains a function that reads n lines
-    of a text file (UTF8) and prints it to stdout.
-"""
+""" This module contains a function that appends to a text file """
 
 
-def read_lines(filename="", nb_lines=0):
-    """Reads n lines of a text file (UTF8) and prints it to stdout.
-    Keyword Arguments:
-        filename {str} -- file name (default: {""})
-        nb_lines {int} -- lines to read (default: {0})
+def append_write(filename="", text=""):
     """
-    with open(filename, mode="r", encoding="utf-8") as file:
-        if nb_lines <= 0:
-            print(file.read(), end="")
-        else:
-            for idx, line in enumerate(file):
-                if idx == nb_lines:
-                    break
-                print(line, end="") 
+    Function that appends to a text file.
+    Args:
+        filename: filename
+        text: text to write
+    Raises:
+        Exception: when the file can be opened
+    """
+
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text) 
