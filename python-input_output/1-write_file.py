6ca9986911892e9ccab30d4689ca2d2c2d5a7971
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-18 02:40:16 -0800

    l

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains a function that returns the number
+     of lines of a text file.
+"""
+
+
+def number_of_lines(filename=""):
+    """Returns the number of lines of a text file.
+    Keyword Arguments:
+        filename {str} -- file name (default: {""})
+    Returns:
+        Int -- number of lines of a text file.
+    """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        return len(file.readlines())
