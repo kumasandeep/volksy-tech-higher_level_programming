873108adaf2ba37b0e9ddcefb9ad773a9ac08c49
kumasandeep <ksandeep@brightcomgroup.com>
2023-01-30 02:22:22 -0800

    k

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,3 @@
 -- mysql
-CREATE TABLE IF NOT EXISTS second_table(id,name,score) Values (1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8) ;
+CREATE TABLE IF NOT EXISTS second_table(id INT ,name VARCHAR(256),score INT)
+insert into second_table Values (1,"John",10),(2,"Alex",3),(3,"Bob",14),(4,"George",8);
