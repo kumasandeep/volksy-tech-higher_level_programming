d574effd275d530ef79dc084f6f59c6da3c41f50
kumasandeep <ksandeep@brightcomgroup.com>
2023-02-02 04:59:03 -0800

    l

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- create db and table like cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE cities(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL,FOREIGN KEY(state_id) REFERENCES states(id));
