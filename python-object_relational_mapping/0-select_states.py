adf136e43f7aac92db9780c067c101d11cc930db
kumasandeep <ksandeep@brightcomgroup.com>
2023-02-08 02:48:56 -0800

    l

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,6 @@ import sys
 import MySQLdb
 
 
-
-
 if __name__ == "__main__":
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
@@ -29,4 +27,3 @@ if __name__ == "__main__":
     cur.close()
 
     conn.close()
-
