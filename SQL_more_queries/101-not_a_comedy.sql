0573e25aec8626c25bba09cb18962e034b114336
kumasandeep <ksandeep@brightcomgroup.com>
2023-02-09 02:26:08 -0800

    l

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,19 @@
--- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
-SELECT DISTINCT tvg.name
-FROM tv_genres tvg
-INNER JOIN tv_show_genres it
-ON tvg.id = it.genre_id
-INNER JOIN tv_shows tvs
-ON tvs.id = it.show_id
-WHERE tvg.name NOT IN (
-      SELECT tvg.name FROM tv_genres tvg
-      INNER JOIN tv_show_genres it
-      ON tvg.id = it.genre_id
-      INNER JOIN tv_shows tvs
-      ON tvs.id = it.show_id
-      WHERE title = 'Dexter')
--- GROUP BY tvg.name
-ORDER BY tvg.name ASC;
+-- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title.
+SELECT DISTINCT `title`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE t.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
+
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
