e24b35cba85f3d2634b10a325ed9106791482676
kumasandeep <ksandeep@brightcomgroup.com>
2023-02-26 23:02:00 -0800

    l

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-// a script that prints the addition of 2 integers
-
+a script that prints the addition of 2 integers
 const a = process.argv[2]; const b = process.argv[3];
 
-if (isNaN(a) || isNaN(b)) 
-{
+if (isNaN(a) || isNaN(b)) {
   console.log('NaN');
-} else 
-{
+} else {
   console.log(Number(a) + Number(b));
 }
