fc6774ffeaa40c8bc3f8b16c5a30b40bdef13a94
kumasandeep <ksandeep@brightcomgroup.com>
2023-05-20 07:50:21 -0700

    l

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-// Update this script by adding a new function incr that increments the integer value.
 const myObject = {
-  type: 'object',
-  value: 12
+  type: 'object', value: 12
 };
 console.log(myObject);
-
 myObject.incr = function () {
-  this.value += 1;
+  myObject.value++;
 };
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
